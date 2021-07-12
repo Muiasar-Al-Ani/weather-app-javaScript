1f8253c5a97c64c77c3bed620d84e0b6642f4009
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-07-12 15:41:14 -0400

    Added the variables for the elements queries that will be used in the script file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var userInput = document.getElementById('user-input')
+var inputForm = document.getElementById('input-form')
+var showingResultsDiv = document.getElementById('showing-results')
+var citiesDiv = document.getElementById('cities')
+
+
