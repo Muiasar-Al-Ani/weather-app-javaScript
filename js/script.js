9caa6f3044b036a99b142ee877ad3596d5124b8a
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-07-12 16:15:04 -0400

    Added a userFormHander function that will take the user input and save it in the local storage.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-var userInput = document.getElementById('user-input')
-var inputForm = document.getElementById('input-form')
+var userInputEl = document.getElementById('user-input')
+var inputFormEl = document.getElementById('input-form')
 var showingResultsDiv = document.getElementById('showing-results')
 var citiesDiv = document.getElementById('cities')
+
+
+
+var userFormHandler = function(event){
+  event.preventDefault();
+
+  var userInput = userInputEl.value.trim();
+
+  if(localStorage.getItem('searchHistory') === null){
+      localStorage.setItem('searchHistory', '[]')
+  }
+
+  var searchHistory = JSON.parse(localStorage.getItem('searchHistory'))
+  searchHistory.unshift(userInput)
+
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+
+}
+
 
 
