e62821ec952390b2c8b0904a7e4dd6cfb73714b0
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-07-13 16:44:21 -0400

    Added a getWeatherApi function that will make an api call and get the weather data needed

--- js/script.js
+++ js/script.js
@@ -1,38 +1,53 @@
-var userInputEl = document.getElementById('user-input')
-var inputFormEl = document.getElementById('input-form')
-var showingResultsDiv = document.getElementById('showing-results')
-var citiesDiv = document.getElementById('cities')
+var userInputEl = document.getElementById("user-input");
+var inputFormEl = document.getElementById("input-form");
+var showingResultsDiv = document.getElementById("showing-results");
+var citiesDiv = document.getElementById("cities");
 
-
-
-var userFormHandler = function(event){
+var userFormHandler = function (event) {
   event.preventDefault();
 
   var userInput = userInputEl.value.trim();
 
-  if(localStorage.getItem('searchHistory') === null){
-      localStorage.setItem('searchHistory', '[]')
+  if (localStorage.getItem("searchHistory") === null) {
+    localStorage.setItem("searchHistory", "[]");
   }
 
-  var searchHistory = JSON.parse(localStorage.getItem('searchHistory'))
-  searchHistory.unshift(userInput)
+  var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+  searchHistory.unshift(userInput);
 
-  localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
-  userInputEl.value = ''
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  userInputEl.value = "";
 
   renderHistory();
-}
+  getWeatherApi(userInput);
+};
 
-var renderHistory = function(){
-    var searchHistory = JSON.parse(localStorage.getItem('searchHistory'))
+var renderHistory = function () {
+  var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+
+  citiesDiv.innerHTML = "";
+  for (var i = 0; i < searchHistory.length; i++) {
+    citiesDiv.innerHTML += `<button class="btn btn-primary w-100 my-1" type="button">${searchHistory[i]}</button>`;
+  }
+};
+
+var getWeatherApi = function (userInput) {
+  var requestUrl =
+    "api.openweathermap.org/data/2.5/forecast?q=" +
+    userInput +
+    "&units=imperial&appid=09ce67c28c7fdad99dc9f81de13032bb";
+
+    fetch(requestUrl).then(function(response){
+        return response.json()
+    }).then(function(data){
+        renderJumbotron(data)
+    })
+};
 
 
-    citiesDiv.innerHTML = ''
-    for (var i = 0; i < searchHistory.length; i++){
-        citiesDiv.innerHTML += `<button class="btn btn-primary w-100 my-1" type="button">${searchHistory[i]}</button>`
-    }
-   
-} 
 
+// 09ce67c28c7fdad99dc9f81de13032bb
 
-inputFormEl.addEventListener('submit', userFormHandler)
+//  api.openweathermap.org/data/2.5/forecast?q=raleigh&appid=09ce67c28c7fdad99dc9f81de13032bb
+
+inputFormEl.addEventListener("submit", userFormHandler);
