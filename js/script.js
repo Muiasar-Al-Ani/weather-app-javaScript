0bd45fbc4d44b4074bdda6b54695234b3eed0f75
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-07-14 12:35:47 -0400

    Added renderFiveDaysForecast function that will create 5 card for each day weather forcast

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var userInputEl = document.getElementById("user-input");
 var inputFormEl = document.getElementById("input-form");
 var showingResultsDiv = document.getElementById("showing-results");
 var citiesDiv = document.getElementById("cities");
+var apiKey = "09ce67c28c7fdad99dc9f81de13032bb";
 
 var userFormHandler = function (event) {
   event.preventDefault();
@@ -37,7 +38,8 @@ var getWeatherApi = function (userInput) {
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
     userInput +
-    "&units=imperial&appid=09ce67c28c7fdad99dc9f81de13032bb";
+    "&units=imperial&appid=" +
+    apiKey;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -50,50 +52,62 @@ var getWeatherApi = function (userInput) {
 
 var renderJumbotron = function (data) {
   showingResultsDiv.innerHTML = `
-    <div class="jumbotron">
+    <div class="jumbotron p-3">
     <h1 class="display-4">${data.city.name} (${moment
     .unix(data.list[0].dt)
     .format("MM/DD/YYYY")})</h1>
     <p class="lead">Temp: ${data.list[0].main.temp} &#8457; <br>Wind: ${
     data.list[0].wind.speed
-  } MPH <br>Humidity: ${data.list[0].main.humidity}% <br> UV index: <span id='uvIndexEl'></span></p>
+  } MPH <br>Humidity: ${
+    data.list[0].main.humidity
+  }% <br> UV index: <span id='uvIndexEl'></span></p>
     
   </div>
-  <div id="fiveDaysForcast"></div>`;
-  renderUvIndex(
-    data.city.coord.lat,
-    data.city.coord.lon
-  )
-  renderFiveDaysForcast(data);
+  <div id="fiveDaysForecast" class="row d-flex justify-content-around p-3"></div>`;
+  renderUvIndex(data.city.coord.lat, data.city.coord.lon);
+  renderFiveDaysForecast(data);
   document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${data.city.name}')`;
 };
 
 var renderUvIndex = function (lat, lon) {
-  console.log(lat);
-  var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=09ce67c28c7fdad99dc9f81de13032bb`;
+  var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
- 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-
       var uvIndex = data.current.uvi;
-      document.getElementById('uvIndexEl').innerHTML = uvIndex < 3
-    ? `<button type="button" class="btn btn-success">${uvIndex}</button>`
-    : uvIndex > 6
-    ? `<button type="button" class="btn btn-warning">${uvIndex}</button>`
-    : `<button type="button" class="btn btn-danger">${uvIndex}</button>`;
-
+      document.getElementById("uvIndexEl").innerHTML =
+        uvIndex < 3
+          ? `<button type="button" class="btn btn-success">${uvIndex}</button>`
+          : uvIndex < 6
+          ? `<button type="button" class="btn btn-warning">${uvIndex}</button>`
+          : `<button type="button" class="btn btn-danger">${uvIndex}</button>`;
     });
-  
-   
 };
 
+var renderFiveDaysForecast = function (data) {
+  var neededWeather = [1, 6, 14, 22, 30];
+  for (var i = 0; i < neededWeather.length; i++) {
+    var icon = `https://openweathermap.org/img/w/${
+      data.list[neededWeather[i]].weather[0].icon
+    }.png`;
 
-
+    document.getElementById("fiveDaysForecast").innerHTML += `
+    <div class="card col-2" ">
+    <div class="card-body">
+      <h5 class="card-title">${moment
+        .unix(data.list[neededWeather[i]].dt)
+        .format("MM/DD/YYYY")}</h5><br>
+        <img src="${icon}"/>
+        <p>Temp: ${data.list[neededWeather[i]].main.temp} &#8457; <br>
+        Wind: ${data.list[neededWeather[i]].wind.speed} MPH <br>
+        Humidity: ${data.list[neededWeather[i]].main.humidity}%</p>
+    </div>
+  </div>`;
+  }
+};
 // 09ce67c28c7fdad99dc9f81de13032bb
 
 //  api.openweathermap.org/data/2.5/forecast?q=raleigh&appid=09ce67c28c7fdad99dc9f81de13032bb
